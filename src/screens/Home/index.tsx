e810ccfadd0e9f7214e18f8494054c430ff7aa34
Vinícius Gurski Ferraz <vinigurskiferraz@gmail.com>
2022-07-04 18:04:32 -0300

    building the cards

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { StatusBar } from "react-native";
 
+import { Car } from "../../components/car";
+
 import {
     Container,
     Header,
@@ -12,6 +14,16 @@ import Logo from '../../assets/logo.svg';
 import { RFValue } from "react-native-responsive-fontsize";
 
 export function Home() {
+    const carData = {
+        brand: 'Audi',
+        name: 'RS 5 Coupé',
+        rent: {
+            period: 'Ao dia',
+            price: 120
+        },
+        thumbnail: 'https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png'
+    }
+
     return (
         <Container>
             <StatusBar
@@ -31,6 +43,8 @@ export function Home() {
                     </TotalCars>
                 </HeaderContent>
             </Header>
+
+            <Car data={carData} />
         </Container>
     );
 }

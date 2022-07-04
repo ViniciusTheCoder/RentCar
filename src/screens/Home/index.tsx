870282f54c62f3b9d5ca3789ddba7fe3b8398844
Vinícius Gurski Ferraz <vinigurskiferraz@gmail.com>
2022-07-04 16:28:43 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { StatusBar } from "react-native";
+
+import {
+    Container,
+    Header,
+    HeaderContent,
+    TotalCars
+} from "./styles";
+
+import Logo from '../../assets/logo.svg';
+import { RFValue } from "react-native-responsive-fontsize";
+
+export function Home() {
+    return (
+        <Container>
+            <StatusBar
+                barStyle={"light-content"}
+                backgroundColor="transparent"
+                translucent
+            />
+            <Header>
+                <HeaderContent>
+                    <Logo
+                        width={RFValue(108)}
+                        heigt={RFValue(12)}
+                    />
+
+                    <TotalCars>
+                        Total 12 carros
+                    </TotalCars>
+                </HeaderContent>
+            </Header>
+        </Container>
+    );
+}

843daa70c3ef48ab1cc481da736904daf5a1ce45
Vinícius Gurski Ferraz <vinigurskiferraz@gmail.com>
2022-07-15 20:14:09 -0300

    buildin' mycars screen

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { DTO } from '../../dtos/CarDTO';
+import { api } from '../../services/api';
 
 import {
     Container
@@ -6,6 +8,22 @@ import {
     from './styles';
 
 export function MyCars() {
+    const [cars, setCars] = useState<DTO[]>([]);
+    const [loading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchCars() {
+            try {
+                const response = await api.get('/schedules_byuser?user_id=1');
+                setCars(response.data)
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+    }, [])
+
     return (
         <Container>
 

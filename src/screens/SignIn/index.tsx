5aa28521b6ec900773133cc8880a4d849e457e40
Vinícius Gurski Ferraz <vinigurskiferraz@gmail.com>
2022-08-02 16:56:39 -0300

    chore: improvin' signIn screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
@@ -19,54 +19,58 @@ import {
 
 export function SignIn() {
     return (
-        <Container>
-            <StatusBar
-                barStyle='dark-content'
-                backgroundColor="transparent"
-                translucent
-            />
-            <Header>
-                <Title>Estamos{'\n'}quase lá.</Title>
-                <Subtitle>
-                    Faça seu login para começar{'\n'}
-                    uma experiência incrível
-                </Subtitle>
-            </Header>
+        <KeyboardAvoidingView behavior='position' enabled>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <Container>
+                    <StatusBar
+                        barStyle='dark-content'
+                        backgroundColor="transparent"
+                        translucent
+                    />
+                    <Header>
+                        <Title>Estamos{'\n'}quase lá.</Title>
+                        <Subtitle>
+                            Faça seu login para começar{'\n'}
+                            uma experiência incrível
+                        </Subtitle>
+                    </Header>
 
-            <Form>
-                <Input
-                    iconName='mail'
-                    placeholder='E-mail'
-                    keyboardType='email-address'
-                    autoCorrect={false}
-                    autoCapitalize='none'
-                />
+                    <Form>
+                        <Input
+                            iconName='mail'
+                            placeholder='E-mail'
+                            keyboardType='email-address'
+                            autoCorrect={false}
+                            autoCapitalize='none'
+                        />
 
-                <InputPassword
-                    iconName='lock'
-                    placeholder='Senha'
-                />
+                        <InputPassword
+                            iconName='lock'
+                            placeholder='Senha'
+                        />
 
 
-            </Form>
+                    </Form>
 
-            <Footer>
-                <Button
-                    title='Login'
-                    onPress={() => { }}
-                    enabled={false}
-                    loading={false}
-                />
+                    <Footer>
+                        <Button
+                            title='Login'
+                            onPress={() => { }}
+                            enabled={false}
+                            loading={false}
+                        />
 
-                <Button
-                    title='Criar conta gratuita'
-                    color={theme.colors.success}
-                    light
-                    onPress={() => { }}
-                    enabled={true}
-                    loading={false}
-                />
-            </Footer>
-        </Container>
+                        <Button
+                            title='Criar conta gratuita'
+                            color={theme.colors.success}
+                            light
+                            onPress={() => { }}
+                            enabled={true}
+                            loading={false}
+                        />
+                    </Footer>
+                </Container>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     );
 }

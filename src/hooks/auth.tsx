e1e3faf0d7e2c13a144cf12f53c7d823992eb538
Vinícius Gurski Ferraz <vinigurskiferraz@gmail.com>
2022-08-15 20:37:53 -0300

    fix: sync

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -24,6 +24,7 @@ interface AuthContextData {
     signIn: (credentials: SignInCredentials) => Promise<void>;
     signOut: () => Promise<void>;
     updateUser: (user: User) => Promise<void>;
+    loading: boolean;
 }
 
 interface AuthProviderProps {
@@ -34,6 +35,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
     const [data, setData] = useState<User>({} as User);
+    const [loading, setLoading] = useState(true);
 
     async function signIn({ email, password }: SignInCredentials) {
 
@@ -108,6 +110,7 @@ function AuthProvider({ children }: AuthProviderProps) {
                 const userData = response[0]._raw as unknown as User;
                 api.defaults.headers.common['Authorization'] = `Bearer ${userData.token}`;
                 setData(userData);
+                setLoading(false);
             }
         }
 
@@ -119,7 +122,8 @@ function AuthProvider({ children }: AuthProviderProps) {
             user: data,
             signIn,
             signOut,
-            updateUser
+            updateUser,
+            loading
         }}>
             {children}
         </AuthContext.Provider>

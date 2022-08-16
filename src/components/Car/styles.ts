eda7b2d85aedcb733f5fc8402d6da1dbff571f62
Vinícius Gurski Ferraz <vinigurskiferraz@gmail.com>
2022-08-16 16:37:46 -0300

    chore: applyi' Fast Image lib for Image cache

--- src/components/Car/styles.ts
+++ src/components/Car/styles.ts
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { RectButton } from "react-native-gesture-handler";
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import FastImage from 'react-native-fast-image';
 
 type CardButtonProps = {
     children: ReactNode
@@ -71,7 +72,7 @@ font-size: ${RFValue(15)}px;
 export const Type = styled.View`
 `;
 
-export const CarImage = styled.Image`
+export const CarImage = styled(FastImage)`
 width: 167px;
 height: 85px;
 `;
